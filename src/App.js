f803fea929612e03f908181129381d2523ea3158
gayu53 <rohulgayu53@gmail.com>
2023-07-05 16:56:40 +0530

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Component/Nav';
+
+
+function App() {
+  return (
+    <div className="layout-wrapper">
+       <Nav />
+    </div>
+     
+  );
+}
+
+export default App;
